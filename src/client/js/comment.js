82a8693e4491420b740e5fcfce38d87ee0219f49
yudetamago <iminaimerudesukinisinaide@harahettana.co.jp>
2021-09-08 10:29:44 +0900

    Submit each comment

--- src/client/js/comment.js
+++ src/client/js/comment.js
@@ -1,33 +1,11 @@
-const form = document.getElementById("commentForm");
-const eachArticle = document.getElementById("eachArticle");
-
-const handleSubmit = async (event) => {
+const test = (event) => {
     event.preventDefault();
-    const input = form.querySelector("input")
-    // const input = form.getElementById("clickedDate");
-    const comment = input.value;
-    // const articleId = eachArticle.dataset.id;
 
-    console.log(comment);
-    // console.log(articleId);
+    const input = event.target.querySelector("input");
+    const id = input.id;
+    const value = input.value;
+    console.log(id);
+    console.log(value);
 
-    /*if (comment === "") {
-      return;
-    }
-    const response = await fetch(`/api/videos/${videoId}/comment`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ text }),
-    });
-    if (response.status === 201) {
-      textarea.value = "";
-      const { newCommentId } = await response.json();
-      addComment(text, newCommentId);
-    }*/
-};
-  
-if (form) {
-    form.addEventListener("submit", handleSubmit);
+    console.log("clicked!"); 
 };
