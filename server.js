93dde2d68dfda674a46e9899894f46431c5acfe5
yudetamago <iminaimerudesukinisinaide@harahettana.co.jp>
2021-09-09 10:58:06 +0900

    Fail to store json in req.body, and test to store text in req.body first, but fail

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import "./src/models/Article";
 import express from "express";
 import path from "path";
 import routeRouter from "./src/routeRouter";
-import bodyParser from "body-parser";
+// import bodyParser from "body-parser";
 
 const app = express();
 const PORT = 3000
@@ -12,7 +12,11 @@ const PORT = 3000
 app.set("view engine", "pug");
 app.set("views", path.join(process.cwd(), "/src/views"));
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(express.json());
+app.use(express.text());
+
 app.use("/static", express.static("src/client"));
 app.use("/", routeRouter);
 
--- src/client/js/comment.js
+++ src/client/js/comment.js
@@ -2,19 +2,22 @@ const test = (event) => {
     event.preventDefault();
 
     try {
-      const input = event.target.querySelector("input");
+      /*const input = event.target.querySelector("input");
       let date = input.id;
       let value = input.value;
       console.log(typeof(date, value));
       console.log(date);
-      console.log(value);
+      console.log(value);*/
 
       let response = fetch("/", {
         method: 'POST',
         headers: {
-          'Content-Type': 'application/json'
+          'Content-Type': 'text/html; charset=utf-8',
+          // 'Accept':'application/json'
         },
-        body: JSON.stringify({ date, value })
+        // json: requestData,
+        // body: JSON.stringify({ date: "2021/08/29", value: "abcdedfgs" }),
+        body: "now in comment.js"
       });
       console.log("fetched?");
       return response.ok
