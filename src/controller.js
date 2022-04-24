e418b00b0e593f2036c019f9987093a34251dd14
sarifor <iread1q84@gmail.com>
2022-04-24 15:51:13 +0900

    Add try/catch

--- src/controller.js
+++ src/controller.js
@@ -7,35 +7,40 @@ let articles = [];
 let result = [];
 
 const accessURL = async () => {
-  const baseURL = "https://www.rarejob.com/dna/2021/08/";
-  const client = axios.create({
-    baseURL: baseURL
-  });
-
-  for (var i = 1; i < 5; i++) {
-    const response = await client.get(`/page/${i}`);
-    const $ = cheerio.load(response.data);
-
-    $(".h2.entry-title > a").each((index, item)=>{articles.push(item.attribs)});
-
-    articles.forEach((div) => { // 배열의 각 요소에 대해 함수 실행
-      const path = div.href;
-      const url = `https://www.rarejob.com${path}`;
-      const title = div.title;
-      const date = url.substring(28, 38);
-
-      result.push({
-          url,
-          title,
-          date,
-      });
-
+  try {
+    const baseURL = "https://www.rarejob.com/dna/2021/08/";
+    const client = axios.create({
+      baseURL: baseURL
     });
 
-    articles = [];
-     
-  }
+    for (var i = 1; i < 5; i++) {
+      const response = await client.get(`/page/${i}`);
+      const $ = cheerio.load(response.data);
 
+      $(".h2.entry-title > a").each((index, item)=>{articles.push(item.attribs)});
+
+      articles.forEach((div) => { // 배열의 각 요소에 대해 함수 실행
+        const path = div.href;
+        const url = `https://www.rarejob.com${path}`;
+        const title = div.title;
+        const date = url.substring(28, 38);
+
+        result.push({
+            url,
+            title,
+            date,
+        });
+
+      });
+
+      articles = [];
+
+    }
+     
+  } catch (e) {
+    console.log(e);
+  };
+  
 };
 
 export const getData = async (req, res) => {
